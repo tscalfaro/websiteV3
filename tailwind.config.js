6054114de1cf45af98703a7025bbb0664082bcf9
Tony Scalfaro <tscalfaro7@gmail.com>
2022-03-08 18:27:46 -0500

    wrapping soon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
 module.exports = {
-  content: [],
+  content: [
+    './index.html',
+    './components/*'
+  ],
+  darkmode: false,
   theme: {
+    textColor: {
+      'primary': '#F4F4E0',
+      'secondary': '#037E7E',
+    },
     colors: {
       'secondary': '#037E7E',
-      'bg': '1F1E1E',
+      'body': '#1F1E1E',
       'bg_gradient': '#141414',
-      'accent': 'C0274C',
-      'primary': 'F4F4E0',
+      'accent': '#C0274C',
+      'primary': '#F4F4E0',
     },
     fontFamily: {
-      'popins': ["'Poppins'", 'sans-serif']
+      'poppins': ["'Poppins'", 'sans-serif']
     },
     extend: {},
   },
