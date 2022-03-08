e795eec64684aae8204d3a888b22b6e88f08b544
Tony Scalfaro <tscalfaro7@gmail.com>
2022-03-08 13:39:44 -0500

    setting up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      'secondary': '#037E7E',
+      'bg': '1F1E1E',
+      'bg_gradient': '#141414',
+      'accent': 'C0274C',
+      'primary': 'F4F4E0',
+    },
+    fontFamily: {
+      'popins': ["'Poppins'", 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
