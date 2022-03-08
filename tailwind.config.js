8424d005e7572c78e855024438a64adac657bbf3
Tony Scalfaro <tscalfaro7@gmail.com>
2022-03-08 18:56:03 -0500

    background color gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,11 @@ module.exports = {
   ],
   darkmode: false,
   theme: {
-    textColor: {
-      'primary': '#F4F4E0',
-      'secondary': '#037E7E',
-    },
+    
     colors: {
       'secondary': '#037E7E',
-      'body': '#1F1E1E',
-      'bg_gradient': '#141414',
+      'body': '#141414',
+      'black': '#0C0C0C',
       'accent': '#C0274C',
       'primary': '#F4F4E0',
     },
