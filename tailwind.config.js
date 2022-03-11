da2d864931fab2b43fe3dd05261c11837c36c7f7
Tony Scalfaro <tscalfaro7@gmail.com>
2022-03-11 13:18:08 -0500

    splash page responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
       'black': '#0C0C0C',
       'accent': '#C0274C',
       'primary': '#FFEEE1',
+      'selected-text': '#936112',
     },
     fontFamily: {
       'poppins': ["'Poppins'", 'sans-serif']
