2a6d37fbbe5bddf4043c5a86894e3fc7997cdde5
Tony Scalfaro <tscalfaro7@gmail.com>
2022-03-18 13:03:57 -0400

    test

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "..", './index.html'))
 })
 
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "./index.html"));
+  });
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
