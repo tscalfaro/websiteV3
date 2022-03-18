12ce18621194652a683cd3362f964e2ad0334781
Tony Scalfaro <tscalfaro7@gmail.com>
2022-03-18 12:47:21 -0400

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 3000;
 const index = require('./index.html')
 
 app.get("", (req, res) => {
-    res.sendFile(index)
+    res.sendFile('./index.html')
 })
 
 app.listen(PORT, () => {
