29dfaa0cd73164391be555e2adf9e40a3a2e61e1
Tony Scalfaro <tscalfaro7@gmail.com>
2022-03-18 12:46:18 -0400

    test

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const app = require("express")()
 const PORT = process.env.PORT || 3000;
-
+const index = require('./index.html')
 
 app.get("", (req, res) => {
-    res.send('hello')
+    res.sendFile(index)
 })
 
 app.listen(PORT, () => {
